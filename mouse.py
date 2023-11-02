9fbe0fc781dd24c60eb3de08cbb8760dc09f584b
Quincy Nickens <quincy.j.nickens@gmail.com>
2023-11-02 14:30:36 -0500

    fixed zoom and navigate functions, highlight hex when mouse hovers over it

--- mouse.py
+++ mouse.py
@@ -2,10 +2,10 @@ import pygame
 
 class MousePosition:
     def __init__(self):
-        self.mouse_x = 0
-        self.mouse_y = 0
+        self.x = 0
+        self.y = 0
         self.update_position()
 
     def update_position(self):
-        self.mouse_x = pygame.mouse.get_pos()[0]
-        self.mouse_y = pygame.mouse.get_pos()[1]
+        self.x = pygame.mouse.get_pos()[0]
+        self.y = pygame.mouse.get_pos()[1]
