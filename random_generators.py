9d866f052343c7e8e3a666e991cbae3a8e2c2fd1
Quincy Nickens <quincy.j.nickens@gmail.com>
2023-11-04 20:18:35 -0500

    Completed 500/1000 entries for Star/Planet name suffixes

---
+++ random_generators.py
@@ -0,0 +1,9 @@
+import random
+from library import name_prefix, name_suffix, name_tag
+
+# ---------- RANDOM STAR/PLANET NAME ---------- #
+def random_name (star=False, planet=False, tag=False):
+    pre =
+    suf =
+    tag =
+    blank = ????, ''?
